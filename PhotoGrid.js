4ae0b2e72ba9c4e3c91bc1576e0729cdb0c54f83
iuhoay <iuhoay@gmail.com>
2017-05-16 18:02:21 +0800

    filling blank space

--- PhotoGrid.js
+++ PhotoGrid.js
@@ -63,6 +63,7 @@ class PhotoGrid extends React.Component {
     return (
       <View style = {[ styles.row, { marginBottom: adjustedMargin } ]}>
         { items.map(item => this.props.renderItem(item, itemWidth)) }
+        { itemsPerRow - items.length > 0 && <View style={{width: itemWidth * (itemsPerRow - items.length)}} />}
       </View>
     );
   }
